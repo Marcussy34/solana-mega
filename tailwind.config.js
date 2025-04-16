37477e3a8cca2cc6e7589047cc626a465c900a93
Marcussy34 <marcus.tanchiyau@gmail.com>
2025-04-16 22:58:19 +0900

    added hero ui

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@heroui/theme/plugin')],
+}
+
